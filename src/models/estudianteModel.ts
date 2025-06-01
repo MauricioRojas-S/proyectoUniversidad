1e4743aad1868911ea424818d37ee8e9269877a1
MauricioRojas-S <mauriciorojas.s2012@gmail.com>
2025-05-31 22:07:01 -0500

    Agregar archivos iniciales: src, .env, app.ts y db.ts

---
+++ src/models/estudianteModel.ts
@@ -0,0 +1,10 @@
+export interface Estudiante {
+    cod_e: number;
+    nom_e: string;
+    dir_e: string;
+    tel_e: number;
+    fecha_nac: Date;
+
+}
+
+// crea la interfaz para mapear lo que trae la base de datos
