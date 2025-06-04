c772828b77c6d7c01f1ce31c738c1e3c566af81f
MauricioRojas-S <mauriciorojas.s2012@gmail.com>
2025-06-04 11:45:23 -0500

    metodos de esturiantes

--- src/controllers/estudianteController.ts
+++ src/controllers/estudianteController.ts
@@ -1,19 +1,16 @@
 import { Estudiante } from '../models/estudianteModel';
 import { db } from '../../db';
 import { OkPacket, RowDataPacket } from 'mysql2';
+import { AnyARecord } from 'dns';
  
 export const create = (estudiante: Estudiante, callback: Function) => {
-    const queryString = 'INSERT INTO estudiantes (cod_e, nom_e, dir_e, tel_e, fech_nac) VALUES (?, ?, ?, ?, ?)';
+    const queryString = 'INSERT INTO estudiantes (cod_e, nom_e, dir_e, tel_e, fecha_nac) VALUES (?, ?, ?, ?, ?)';
  
     db.query(
         queryString,
         [estudiante.cod_e, estudiante.nom_e, estudiante.dir_e, estudiante.tel_e, estudiante.fecha_nac],
         (err) => {
             if (err) { callback(err); }
- 
-            //const insertId = (<OkPacket>result).insertId;
-            //callback(null, insertId);
- 
             callback(null, {
                 statusCode: 201,
                 message: 'Estudiante creado exitosamente',
@@ -23,4 +20,93 @@ export const create = (estudiante: Estudiante, callback: Function) => {
             });
         }
     );
+};
+
+export const getAll = (callback: Function) => {
+    const queryString = "SELECT * FROM estudiantes";
+    db.query(queryString, (err, results: any) => {
+        if (err) {
+            callback(err);
+        } else {
+            callback(null, {
+                statusCode: 200,
+                message: 'Estudiantes encontrados exitosamente',
+                data: results[0]
+            });
+        }
+    });
+};
+
+export const getOne = (cod_e: string, callback: Function) => {
+  const queryString = 'SELECT * FROM estudiantes WHERE cod_e = ?';
+
+  db.query(queryString, [cod_e], (err, results: any) => {
+    if (err) {
+      callback(err);
+    } else if (results.length === 0) {
+      callback(null, {
+        statusCode: 404,
+        message: 'Estudiante no encontrado',
+        data: null
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        message: 'Estudiante encontrado',
+        data: results[0]
+      });
+    }
+  });
+};
+
+export const update = (cod_e: string, estudiante: Estudiante, callback: Function) => {
+  const queryString = `UPDATE estudiantes SET nom_e = ?, dir_e = ?, tel_e = ?, fecha_nac = ? WHERE cod_e = ?`;
+
+  db.query(
+    queryString,
+    [estudiante.nom_e, estudiante.dir_e, estudiante.tel_e, estudiante.fecha_nac, cod_e],
+    (err, result: any) => {
+      if (err) {
+        callback(err);
+      } else if (result.affectedRows === 0) {
+        callback(null, {
+          statusCode: 404,
+          message: 'Estudiante no encontrado',
+          data: null,
+        });
+      } else {
+        callback(null, {
+          statusCode: 200,
+          message: 'Estudiante actualizado exitosamente',
+          data: {
+            cod_e: cod_e,
+          },
+        });
+      }
+    }
+  );
+};
+
+export const remove = (cod_e: string, callback: Function) => {
+  const queryString = 'DELETE FROM estudiantes WHERE cod_e = ?';
+
+  db.query(queryString, [cod_e], (err, result: any) => {
+    if (err) {
+      callback(err);
+    } else if (result.affectedRows === 0) {
+      callback(null, {
+        statusCode: 404,
+        message: 'Estudiante no encontrado',
+        data: null,
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        message: 'Estudiante eliminado exitosamente',
+        data: {
+          cod_e: cod_e,
+        },
+      });
+    }
+  });
 };
--- src/routes/estudianteRouter.ts
+++ src/routes/estudianteRouter.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import * as estudianteController from '../controllers/estudianteController';
 import { Estudiante } from '../models/estudianteModel';
+
 const estudianteRouter = express.Router();
  
 estudianteRouter.post('/', async (req: Request, res: Response) => {
@@ -13,5 +14,53 @@ estudianteRouter.post('/', async (req: Request, res: Response) => {
         res.status(result.statusCode).json(result);
     });
 });
+
+estudianteRouter.get('/:cod_e', async (req: Request, res: Response) => {
+  const cod_e = req.params.cod_e;
+
+  estudianteController.getOne(cod_e, (err: Error, result: any) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.status(result.statusCode).json(result);
+  });
+});
+
+estudianteRouter.get('/', async (req: Request, res: Response) => {
+    estudianteController.getAll((err: Error, result: any) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else {
+            res.status(result.statusCode).json(result);
+        }
+    });
+});
+
+estudianteRouter.put('/:cod_e', async (req: Request, res: Response) => {
+  const cod_e = req.params.cod_e;
+  const updatedEstudiante: Estudiante = req.body;
+
+  estudianteController.update(cod_e, updatedEstudiante, (err: Error, result: any) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.status(result.statusCode).json(result);
+  });
+});
+
+estudianteRouter.delete('/:cod_e', async (req: Request, res: Response) => {
+  const cod_e = req.params.cod_e;
+
+  estudianteController.remove(cod_e, (err: Error, result: any) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.status(result.statusCode).json(result);
+  });
+});
+
 
 export {estudianteRouter};
